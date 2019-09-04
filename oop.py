ed63d54f2766fad41c281a5c4386340a4469b5de
amarasm <48725326+amarasm@users.noreply.github.com>
2019-09-04 13:52:17 +0400

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,36 +1,51 @@
-class Grade:
-    min = 0
-    max = 100
+class Student:
+    Participation = 0.10
+    Homework = 0.30
+    Quizzes = 0.20
+    FinalProject = 0.40
 
-    def __init__(self, mark=-1):
-        if (mark == -1):
-            self.mark = Grade.askUserInput()
-        else:
-            self.mark = mark
+    def __init__(self, ID, firstName, lastName, phone, gender, birthdate, participation, homework, quizzes, finalProject):
+        self.ID = ID
+        self.fullName = firstName + " " + lastName
+        self.email = firstName + "." + lastName + "@aua.am"
+        self.phone = phone
+        self.gender = gender
+        self.birthdate = birthdate
+        self.participation = participation
+        self.homework = homework
+        self.quizzes = quizzes
+        self.finalProject = finalProject
 
-    @classmethod
-    def askUserInput(cls):
-        return int(input("please insert the grade"))
 
-    def display(self):
-        if (self.isValid()):
-            print(self.mark)
-        else:
-            print("invalid mark")
+    def getPersonalInfo(self):
+        print(self.ID)
+        print(self.fullName)
+        print(self.email)
+        print(self.phone)
+        print(self.gender)
+        print(self.birthdate)
+        print("----------------------")
 
-    def isValid(self):
-        return Grade.min <= self.mark and self.mark <= Grade.max
+
+    def getCurrentGrade(self):
+        finalParticipation = float(self.Participation * self.participation)
+        finalHomework = float(self.Homework * self.homework)
+        finalQuizzes = float(self.Quizzes * self.quizzes)
+        finalProject = float(self.FinalProject * self.finalProject)
+        finalGrade = float(finalParticipation + finalHomework + finalQuizzes + finalProject)
+        print(finalGrade)
+
+
 
 def main():
-    gr1 = Grade(85)
-    gr1.display()
+    st1 = Student("AUA123", "User1", "no1", "123456", "M", "01/01/2019", 100, 90, 92, 100)
+    st2 = Student("AUA123", "User2", "no2", "654321", "F", "01/01/2018", 87, 93, 100, 98)
+
+    st1.getPersonalInfo()
+    st2.getPersonalInfo()
+    st1.getCurrentGrade()
+    st2.getCurrentGrade()
 
 
-    gr2 = Grade(185)
-    gr2.display()
-
-
-    gr3 = Grade()
-    gr3.display()
 
 main()
