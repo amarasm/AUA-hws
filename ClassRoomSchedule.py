e0e129d42d9e0dec97e1844df748039fdb3cf12b
amarasm <48725326+amarasm@users.noreply.github.com>
2019-09-12 19:32:26 +0400

    Update ClassRoomSchedule.py

--- ClassRoomSchedule.py
+++ ClassRoomSchedule.py
@@ -10,10 +10,15 @@ class Classroom:
         print("the location of the class is", self.classnumber + self.eastWestM)
 
     def classIsFree(self, start, end):
-        if (self.schedule.cl1[0] >= start and self.schedule.cl1[0] >= start) or (self.schedule.cl1[1] <= end and self.schdule.cl1[1] <= end):
-            return False
-        else:
-            return True
+        isFree = True
+        for slot in self.schedule:
+            if (slot["start"] >= start and slot["end"] <= start) or (slot["start"] <= end and slot["end"] <= end):
+                continue
+            else:
+                isFree = False
+                break
+        return isFree
+
 
     def bookClassRoom(self, start, end):
         if (self.classIsFree(start, end)):
@@ -27,18 +32,20 @@ class Classroom:
 
 def main():
     myclass = Classroom(100, "208", "East")
-    cl1 = Classroom(100, "208", "East")
+
 
     start = input("Please enter the start time")
     end = input("Please enter the end time")
-    cl1.schedule.append({"start": 1235, "end":1467})
+   
 
-    while not(myclass.bookClassRoom(start, end)):
-        print("This classroom is booked for the interval you have entered. Please choose another interval")
-        start = input("Please enter the start time")
-        end = input("Please enter the end time")
-
-    print(myclass.schedule)
+    while True:
+        if myclass.bookClassRoom(start, end) == False:
+            print("This classroom is booked for the interval you have entered. Please choose another interval")
+            start = input("Please enter the start time")
+            end = input("Please enter the end time")
+        else:
+            print(myclass.schedule)
+            break
 
 
 main()
